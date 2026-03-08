a18f99a0700645089dffba89b45c468b7a1e693c
Zibrian Cadinot <zcadinot@c2r3p5.42lehavre.fr>
2026-03-08 23:47:04 +0100

    Fix True bin

--- src/persistance/true.cpp
+++ src/persistance/true.cpp
@@ -6,7 +6,7 @@
 /*   By: zcadinot <zcadinot@student.42lehavre.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/08 22:57:04 by zcadinot          #+#    #+#             */
-/*   Updated: 2026/03/08 23:39:32 by zcadinot         ###   ########.fr       */
+/*   Updated: 2026/03/08 23:46:45 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
     (void)argc;
     (void)argv;
     run_bin();
-    std::cout << "trueeeeeeeeee" << std::endl;
+    /* std::cout << "verification msg" << std::endl; */
     exit(0);
     return (0);
 }
